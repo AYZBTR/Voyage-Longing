6e5022f759ac8effd06f99657cfb35b07e0a8dcc
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-12-26 18:30:43 +0200

    initializing databse done

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+/* const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -10,8 +10,35 @@ const listingSchema = new Schema({
 
     image: {
         type: String,
+        filename: String,
         default: "https://images.unsplash.com/photo-1460627390041-532a28402358?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v) => v === "" ? "https://images.unsplash.com/photo-1460627390041-532a28402358?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+ */
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+
+    image: {
+        type: {
+            url: { type: String, default: "https://images.unsplash.com/photo-1460627390041-532a28402358?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+            filename: { type: String, default: "default-image" },
+        },
+        default: {},
     },
     price: Number,
     location: String,
