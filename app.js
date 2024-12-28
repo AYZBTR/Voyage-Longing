99c38d206bba7a2875b9e9f1c9a465e28a6fbd25
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-12-28 16:41:09 +0200

    index route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/voyagelonging";
 
@@ -17,11 +18,21 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("Hi, from Root!");
 });
 
-app.get("/testListing", async (req, res) => {
+//index route
+app.get("/listings", async (req,res)=>{
+    const allListings= await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+        
+});
+
+/* app.get("/testListing", async (req, res) => {
   let sampleListing = new Listing({
     title: "My New Villa",
     description: "By the beach",
@@ -33,7 +44,7 @@ app.get("/testListing", async (req, res) => {
   await sampleListing.save(); //Saves this document to the MongoDB databse.
   console.log("Sample was saved");
   res.send("successfull testing")
-});
+}); */
 
 app.listen(8080, () => {
   console.log("Server is listening to port 8080!");
