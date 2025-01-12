38a8696f8d4e9b9c1080071f43a4c95c8192b0ab
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-01-12 14:06:41 +0200

    express error in  action

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/voyagelonging";
 
@@ -86,7 +87,8 @@ app.delete("/listings/:id", async (req, res) => {
 
 
 app.use((err, req, res, next) => {
-  res.send("something went wrong");
+  let {statusCode, message} = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
