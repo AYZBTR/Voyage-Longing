ae3f694805622d2d642e3ac838108aa084fd9c4c
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-01-23 03:06:55 +0200

    server side validation for reviews using joi

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/voyagelonging";
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
   res.send("Hi, from Root!");
 });
 
-
+//Validating Listing
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
   if (error) {
@@ -44,8 +44,18 @@ const validateListing = (req, res, next) => {
   } else {
     next();
   }
-}
+};
 
+//validate user reveiws
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 
 
 //index route
@@ -100,7 +110,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 
 //Reveiws
 //Post Route
-app.post("/listings/:id/reviews", async(req,res)=>{
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res)=>{
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
 
@@ -108,7 +118,7 @@ app.post("/listings/:id/reviews", async(req,res)=>{
   await newReview.save();
   await listing.save();
   res.redirect(`/listings/${listing._id}`);
-})
+}));
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
