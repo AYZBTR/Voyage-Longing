5b694996cade612d6950e6d784e29fb3bf5580c6
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-01-23 02:18:30 +0200

    creating route for reviews. /listings/:id/reviews

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const Listing = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js")
-const ExpressError = require("./utils/ExpressError.js")
-const { listingSchema } = require("./schema.js")
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/voyagelonging";
 
@@ -96,6 +97,19 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }));
+
+//Reveiws
+//Post Route
+app.post("/listings/:id/reviews", async(req,res)=>{
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.reveiw);
+
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  console.log("new review saved");
+  res.send("new review saved")
+})
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
