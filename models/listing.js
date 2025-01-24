8265cf80b62e8ce13c99d7eaba190695bbc0a1ac
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-01-25 00:26:02 +0200

    handeling : Delete listing

--- models/listing.js
+++ models/listing.js
@@ -1,26 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("/review.js");
 
 const listingSchema = new Schema({
   title: {
     type: String,
     required: true,
   },
-/*   description: String,
-    image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        : v,
-  },   */
+  /*   description: String,
+      image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          : v,
+    },   */
   description: String,
   image: {
     url: String,
     filename: String
-  },  
+  },
   price: Number,
   location: String,
   country: String,
@@ -29,8 +30,15 @@ const listingSchema = new Schema({
       type: Schema.Types.ObjectId,
     }
   ] */
-    reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
 });
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+
+})
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
