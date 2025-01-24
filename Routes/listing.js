ba5f5f1fe8e23bca2f089187319b03bc2156ea59
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-01-25 00:58:44 +0200

    Express router restructuring

---
+++ Routes/listing.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+
+
+//index route
+app.get("/listings", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  
+  }));
+  
+  
+  //New Route
+  app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs")
+  });
+  
+  //Show Route
+  app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews").exec();
+    res.render("listings/show.ejs", { listing });
+  }));
+  
+
+  //Create Route
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  }));
+  
+  //Edit Route
+  app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing })
+  }));
+  
+  //Update Route'
+  app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`)
+  }));
+  
+  
+  //Delete Route
+  app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  }));
+  
