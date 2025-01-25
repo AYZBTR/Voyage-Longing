c501bc2161d4e7430a7b832e05173cb91f7595ca
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-01-26 01:43:54 +0200

    using express router

--- Routes/listing.js
+++ Routes/listing.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("./utils/wrapAsync.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const ExpressError = require("./utils/ExpressError.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const { listingSchema, reviewSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Listing = require("../models/listing.js")
 
+
+
+//Validating Listing
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+      let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    } else {
+      next();
+    }
+  };
 //index route
 router.get("/", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
--- app.js
+++ app.js
@@ -37,16 +37,7 @@ app.get("/", (req, res) => {
   res.send("Hi, from Root!");
 });
 
-//Validating Listing
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+
 
 //validate user reveiws
 const validateReview = (req, res, next) => {
