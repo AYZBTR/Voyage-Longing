4418095ad558a6b3bdc12db3c6da9f4d7ffdb32b
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-01-27 00:14:58 +0200

    implementing sessions

--- Routes/review.js
+++ Routes/review.js
@@ -22,7 +22,7 @@ const validateReview = (req, res, next) => {
 
 //Post review Route
 router.post("/", validateReview, wrapAsync(async (req, res) => {
-  console.log(req.params.id);
+  
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
   listing.reviews.push(newReview);
